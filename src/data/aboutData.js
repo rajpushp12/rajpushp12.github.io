4fa578164fe13eb4af4c9baf7373da4835c0d7b8
Pushp Raj <pushpraj@Pushps-MacBook-Air.local>
2022-02-03 15:56:49 +0530

    contacts api

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "I'm a Tech - Marketing enthusiast pursuing B.Tech focused in Electronics and Communication Engineering from MIT, Manipal. I am proficient at coding in Java as well as Python and I've been learning Microsoft Azure Cloud Services.",
+    description1: "I'm a Tech - Marketing enthusiast pursuing B.Tech focused in Electronics and Communication Engineering from MIT, Manipal. I'm learning Microsoft Azure Cloud Services and I am proficient at coding in Java as well as Python.",
     image: 2
 }
